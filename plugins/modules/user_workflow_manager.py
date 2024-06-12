1f6c2dbe2eadd6690011be94a0e0a6de17720f3b
Ajith Andrew J <ajith.andrew@ltts.com>
2024-06-12 19:30:13 +0530

    validation method updated in user_workflow_manager.py

--- plugins/modules/user_workflow_manager.py
+++ plugins/modules/user_workflow_manager.py
@@ -22,7 +22,7 @@ from ansible_collections.cisco.dnac.plugins.module_utils.dnac import (
 from ansible.module_utils.basic import AnsibleModule
  
 class UserWorkflowManager(DnacBase):
-    """Class containing member attributes for DNAC Access Point Automation module"""
+    """Class containing member attributes for DNAC User Automation module"""
  
     def __init__(self, module):
         super().__init__(module)
@@ -30,16 +30,84 @@ class UserWorkflowManager(DnacBase):
         self.supported_states = ["merged"]
         self.payload = module.params
         self.keymap = {}
-        self.baseurl = "https://" + module.params["dnac_host"]+ ":" + module.params["dnac_port"]
+        self.baseurl = "https://" + module.params["dnac_host"] + ":" + module.params["dnac_port"]
         self.log('Login DNAC using by user: ' + module.params["dnac_username"], "INFO")
         try:
             self.dnacsdk = api.DNACenterAPI(base_url=self.baseurl,
-                                    username = module.params["dnac_username"],
-                                    password = module.params["dnac_password"],
-                                    verify = False)
+                                            username=module.params["dnac_username"],
+                                            password=module.params["dnac_password"],
+                                            verify=False)
+        except Exception as e:
+            self.log("Unable to Login DNAC " + str(e), "ERROR")
+ 
+    # Below function used to validate input over the ansible validation
+    def validate_input_yml(self, inputdata):
+        self.log('Validating the Yaml File..', "INFO")
+        try:
+            if not isinstance(inputdata, dict):
+                raise ValueError("Expected inputdata to be a dictionary")
+ 
+            errormsg = []
+            userlist = inputdata.get("config", {}).get("update_users", [])
+            if not userlist:
+                errormsg.append("No users provided in the 'update_users' list.")
+            for eachuser in userlist:
+                if not isinstance(eachuser, dict):
+                    errormsg.append("Each user entry must be a dictionary.")
+                    continue
+ 
+                temp_spec = dict(eachuser=dict(type='dict'))
+                eachuser = self.camel_to_snake_case(eachuser)
+ 
+                user_info = dict(firstName=dict(type='str'),
+                                 lastName=dict(type='str'),
+                                 email=dict(type='str'),
+                                 username=dict(type='str'),
+                                 userId=dict(type='str'),
+                                 roleList=dict(type='list', elements='str'),
+                                 )
+ 
+                valid_param, invalid_param = validate_list_of_dicts(eachuser, user_info)
+                if invalid_param:
+                    errormsg.append(f"Invalid param found '{', '.join(invalid_param)}' in input")
+ 
+                if eachuser.get("firstName"):
+                    param_spec = dict(type="str", length_max=32)
+                    validate_str(eachuser["firstName"], param_spec, "firstName", errormsg)
+ 
+                if eachuser.get("lastName"):
+                    param_spec = dict(type="str", length_max=32)
+                    validate_str(eachuser["lastName"], param_spec, "lastName", errormsg)
+ 
+                if eachuser.get("email"):
+                    param_spec = dict(type="str", length_max=255)
+                    validate_str(eachuser["email"], param_spec, "email", errormsg)
+ 
+                if eachuser.get("username"):
+                    param_spec = dict(type="str", length_max=255)
+                    validate_str(eachuser["username"], param_spec, "username", errormsg)
+ 
+                if eachuser.get("userId"):
+                    param_spec = dict(type="str", length_max=255)
+                    validate_str(eachuser["userId"], param_spec, "userId", errormsg)
+ 
+                if eachuser.get("roleList"):
+                    if not isinstance(eachuser["roleList"], list):
+                        errormsg.append("roleList must be a list.")
+                    else:
+                        for role in eachuser["roleList"]:
+                            param_spec = dict(type="str", length_max=255)
+                            validate_str(role, param_spec, "roleList item", errormsg)
+ 
+            if errormsg:
+                self.log("Invalid Input in input file: '{0}' ".format(str("\n".join(errormsg))), "ERROR")
+                self.module.fail_json(msg=str("\n".join(errormsg)))
  
         except Exception as e:
-            self.log("Unable to Login DNAC "+ str(e) , "ERROR")
+            self.log("Invalid Param provided in input Yml File. {0}".format(str(e)), "ERROR")
+            self.msg = "Invalid parameters in playbook: {0}".format(str(e))
+            self.status = "failed"
+            return self
  
 def main():
     """ main entry point for module execution
@@ -70,9 +138,11 @@ def main():
     ccc_network = UserWorkflowManager(module)
  
     # Check the Input file should not be empty config param
-    if len(module.params.get('config').get("update_users")) < 1:
+    if not module.params.get('config') or not module.params['config'].get("update_users"):
         module.fail_json(msg='User Should not be Empty, You may forget to pass input.yml',
                          **ccc_network.result)
+ 
+    ccc_network.validate_input_yml(module.params)
  
     module.exit_json(**ccc_network.result)
  
