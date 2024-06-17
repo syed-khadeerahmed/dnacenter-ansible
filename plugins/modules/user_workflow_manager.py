51ac1e4af776c4ed4f20ce96cd728c0378bf30c7
ajithandrewj <ajithandrewj7@gmail.com>
2024-06-17 11:12:14 +0530

    python doc is added in user_workflow_manager.py

--- plugins/modules/user_workflow_manager.py
+++ plugins/modules/user_workflow_manager.py
@@ -12,137 +12,238 @@ __author__ = ("Ajith Andrew J, Syed khadeer Ahmed")
 DOCUMENTATION = r"""
 ---
 module: user
-short_description: Resource module for User
+short_description: Resource module for managing Users in Cisco DNA Center
 description:
-- Manage operations create and update of the resource User.
-- Add a new user for Cisco DNA Center system.
-- Update a user for Cisco DNA Center system.
-- Delete a user for cisco DNA Center system
+  - Manages operations to create, update, and delete users in the Cisco DNA Center system.
+  - Allows adding a new user.
+  - Supports updating an existing user.
+  - Enables deleting a user.
 version_added: '6.7.0'
 extends_documentation_fragment:
   - cisco.dnac.workflow_manager_params
-author: Syed Khadeer Ahmed (@syed-khadeerahmed)
-        Ajith Andrew J(@ajithandrewj)
- 
-Create user options:
-  email:
-    description: Email.
-    type: str
-  firstName:
-    description: First Name.
-    type: str
-  lastName:
-    description: Last Name.
-    type: str
-  password:
-    description: Password.
-    type: str
-  username:
-    description: Username.
-    type: str
-  roleList:
-    description: Role id list.
-    elements: str
-    type: list
- 
-Update user Options
-  email:
-    description: Email. Email should be set if the original value is not empty
-    type: str
-  firstName:
-    description: First Name. FirstName should be set if the original value is not empty
-    type: str
-  lastName:
-    description: Last Name. LastName should be set if the original value is not empty
-    type: str
-  username:
-    description: Username.
-    type: str
-  roleList:
-    description: Role id list.
-    elements: str
-    type: list
- 
+author:
+  - Syed Khadeer Ahmed (@syed-khadeerahmed)
+  - Ajith Andrew J (@ajithandrewj)
+
+options:
+  create_user:
+    email:
+      description: The email address of the user.
+      type: str
+    firstName:
+      description: The first name of the user.
+      type: str
+    lastName:
+      description: The last name of the user.
+      type: str
+    password:
+      description: The password for the user's account.
+      type: str
+    username:
+      description: The username for the user's account.
+      type: str
+    roleList:
+      description: A list of role IDs assigned to the user.
+      elements: str
+      type: list
+
+  update_user:
+    email:
+      description: The email address of the user. This must be set if the original value is not empty.
+      type: str
+    firstName:
+      description: The first name of the user. This must be set if the original value is not empty.
+      type: str
+    lastName:
+      description: The last name of the user. This must be set if the original value is not empty.
+      type: str
+    username:
+      description: The username for the user's account.
+      type: str
+    roleList:
+      description: A list of role IDs assigned to the user.
+      elements: str
+      type: list
+
+  delete_user:
+    username:
+      description: The username of the user to be deleted.
+      type: str
+      required: true
+
 requirements:
-- dnacentersdk >= 2.5.5
-- python >= 3.5
-seealso:
-- name: Cisco DNA Center documentation for User and Roles AddUserAPI
-  description: Complete reference of the AddUserAPI API.
-  link: https://developer.cisco.com/docs/dna-center/#!add-user-api
-- name: Cisco DNA Center documentation for User and Roles UpdateUserAPI
-  description: Complete reference of the UpdateUserAPI API.
-  link: https://developer.cisco.com/docs/dna-center/#!update-user-api
-- name: Cisco DNA Center documentation for User and Roles UpdateUserAPI
-  description: Complete reference of the UpdateUserAPI API.
-  link: https://developer.cisco.com/docs/dna-center/#!delete-user-api
- 
+  - dnacentersdk >= 2.7.1
+  - python >= 3.10
+
+see also:
+  - name: Cisco DNA Center documentation for User and Roles AddUserAPI
+    description: Complete reference of the AddUserAPI API.
+    link: https://developer.cisco.com/docs/dna-center/#!add-user-api
+  - name: Cisco DNA Center documentation for User and Roles UpdateUserAPI
+    description: Complete reference of the UpdateUserAPI API.
+    link: https://developer.cisco.com/docs/dna-center/#!update-user-api
+  - name: Cisco DNA Center documentation for User and Roles DeleteUserAPI
+    description: Complete reference of the DeleteUserAPI API.
+    link: https://developer.cisco.com/docs/dna-center/#!delete-user-api
+
 notes:
-  - SDK Method used are
-    user_and_roles.UserandRoles.add_user_ap_i,
-    user_and_roles.UserandRoles.update_user_ap_i,
-    user_and_roles.UserandRoles.delete_user_ap_i,
- 
-  - Paths used are
-    post /dna/system/api/v1/user,
-    put /dna/system/api/v1/user,
-    delete /dna/system/api/v1/user/{userId}
+  - SDK Methods used:
+    - user_and_roles.UserandRoles.get_user_ap_i
+    - user_and_roles.UserandRoles.add_user_ap_i
+    - user_and_roles.UserandRoles.update_user_ap_i
+    - user_and_roles.UserandRoles.delete_user_ap_i
+    - user_and_roles.UserandRoles.get_roles_ap_i
+  - Paths used:
+    - get /dna/system/api/v1/user
+    - post /dna/system/api/v1/user
+    - put /dna/system/api/v1/user
+    - delete /dna/system/api/v1/user/{userId}
+    - get /dna/system/api/v1/roles
 """
+
 EXAMPLES = r"""
 ---
--name: Create User
-  cisco.dnac.user_workflow_manager:
-    dnac_host: "{{ dnac_host }}"
-    dnac_username: "{{ dnac_username }}"
-    dnac_password: "{{ dnac_password }}"
-    dnac_verify: "{{ dnac_verify }}"
-    dnac_port: "{{ dnac_port }}"
-    dnac_version: "{{ dnac_version }}"
-    dnac_debug: "{{ dnac_debug }}"
-    state: Merged
-    config:
-      email: "syedkhadeer@example.com"
-      firstName: "Syed Khadeer"
-      lastName: "Ahmed"
-      password: "password123"
-      roleList:
-        - "Network Administrator"
-      username: "syed"
- 
--name: Update User
-  cisco.dnac. user_workflow_manager:
-    dnac_host: "{{ dnac_host }}"
-    dnac_username: "{{ dnac_username }}"
-    dnac_password: "{{ dnac_password }}"
-    dnac_verify: "{{ dnac_verify }}"
-    dnac_port: "{{ dnac_port }}"
-    dnac_version: "{{ dnac_version }}"
-    dnac_debug: "{{ dnac_debug }}"
-    state: present
-    config:
-      email: "ajithandrew@example.com"
-      firstName: "Ajith "
-      lastName: "Andrew"
-      roleList:
-        - "system Administrator"
+  - name: Create User
+    cisco.dnac.user_workflow_manager:
+      dnac_host: "{{ dnac_host }}"
+      dnac_username: "{{ dnac_username }}"
+      dnac_password: "{{ dnac_password }}"
+      dnac_verify: "{{ dnac_verify }}"
+      dnac_port: "{{ dnac_port }}"
+      dnac_version: "{{ dnac_version }}"
+      dnac_debug: "{{ dnac_debug }}"
+      state: merged
+      config:
+        email: "syedkhadeer@example.com"
+        firstName: "Syed Khadeer"
+        lastName: "Ahmed"
+        password: "password123"
+        roleList:
+          - "Network Administrator"
+        username: "syed"
+
+  - name: Update User
+    cisco.dnac.user_workflow_manager:
+      dnac_host: "{{ dnac_host }}"
+      dnac_username: "{{ dnac_username }}"
+      dnac_password: "{{ dnac_password }}"
+      dnac_verify: "{{ dnac_verify }}"
+      dnac_port: "{{ dnac_port }}"
+      dnac_version: "{{ dnac_version }}"
+      dnac_debug: "{{ dnac_debug }}"
+      state: present
+      config:
+        email: "ajithandrew@example.com"
+        firstName: "Ajith"
+        lastName: "Andrew"
+        roleList:
+          - "System Administrator"
         username: "ajith"
- 
--name: Delete  User
-  cisco.dnac.user_workflow_manager:
-    dnac_host: "{{ dnac_host }}"
-    dnac_username: "{{ dnac_username }}"
-    dnac_password: "{{ dnac_password }}"
-    dnac_verify: "{{ dnac_verify }}"
-    dnac_port: "{{ dnac_port }}"
-    dnac_version: "{{ dnac_version }}"
-    dnac_debug: "{{ dnac_debug }}"
-    state: absent
-    config:
-      username: "joel"
+
+  - name: Delete User
+    cisco.dnac.user_workflow_manager:
+      dnac_host: "{{ dnac_host }}"
+      dnac_username: "{{ dnac_username }}"
+      dnac_password: "{{ dnac_password }}"
+      dnac_verify: "{{ dnac_verify }}"
+      dnac_port: "{{ dnac_port }}"
+      dnac_version: "{{ dnac_version }}"
+      dnac_debug: "{{ dnac_debug }}"
+      state: absent
+      config:
+        username: "joel"
 """
 
 RETURN = r"""
+# Case 1: User operation successful (create/update/delete)
+response_1:
+  description: A dictionary with details of the API execution from Cisco DNA Center.
+  returned: always
+  type: dict
+  sample: >
+    {
+      "response": {
+        "bapiExecutionId": "string",
+        "bapiKey": "string",
+        "bapiName": "string",
+        "endTime": "string",
+        "endTimeEpoch": 0,
+        "runtimeInstanceId": "string",
+        "userId": "string",  # User ID from Cisco DNA Center
+        "startTime": "string",
+        "startTimeEpoch": 0,
+        "status": "string",  # Status of the operation
+        "timeDuration": 0
+      },
+      "msg": "User operation successful."
+    }
 
+# Case 2: User exists and no action needed (for update)
+response_2:
+  description: A dictionary with existing user details indicating no update needed.
+  returned: always
+  type: dict
+  sample: >
+    {
+      "response": {
+        "user": {
+          "email": "user@example.com",
+          "firstName": "John",
+          "lastName": "Doe",
+          "username": "johndoe",
+          "roleList": ["Network Administrator"]
+          # Additional user details as needed
+        },
+        "userId": "string",  # User ID from Cisco DNA Center
+        "type": "string"
+      },
+      "msg": "User already exists and no update needed."
+    }
+
+# Case 3: Error during user operation (create/update/delete)
+response_3:
+  description: A dictionary with details of the API execution and error information.
+  returned: always
+  type: dict
+  sample: >
+    {
+      "response": {
+        "bapiError": "string",  # Specific error message
+        "bapiExecutionId": "string",
+        "bapiKey": "string",
+        "bapiName": "string",
+        "endTime": "string",
+        "endTimeEpoch": 0,
+        "runtimeInstanceId": "string",
+        "startTime": "string",
+        "startTimeEpoch": 0,
+        "status": "string",
+        "timeDuration": 0
+      },
+      "msg": "Error during creating or updating or deliting the user."
+    }
+
+# Case 4: User not found (during delete operation)
+response_4:
+  description: A dictionary indicating user not found during delete operation.
+  returned: always
+  type: dict
+  sample:
+    {
+      "response": [],
+      "msg": "User not found."
+    }
+
+# Case 5: Role not found (during get operation)
+response_5:
+  description: A dictionary indicating role not found during get operation.
+  returned: always
+  type: dict
+  sample:
+    {
+      "response": [],
+      "msg": "Role not found."
+    }
 """
 
 from ansible.module_utils.basic import AnsibleModule
